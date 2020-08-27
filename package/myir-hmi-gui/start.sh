b0a4559fe452bc5586cab39bc984f9a81337219d
codebug8 <livelwh@outlook.com>
2020-08-27 03:06:01 +0000

    Modfiy bootargs parameter.
    Signed-off-by: codebug8 <livelwh@outlook.com>

--- package/myir-hmi-gui/start.sh
+++ package/myir-hmi-gui/start.sh
@@ -9,10 +9,12 @@ echo  139 > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio139/direction
 echo 1 > /sys/class/gpio/gpio139/value
 
+
 export QT_QPA_EGLFS_ALWAYS_SET_MODE="1"
 export QT_QPA_EGLFS_KMS_ATOMIC='1'
 export QT_QPA_EGLFS_KMS_CONFIG='/usr/share/qt5/cursor.json'
 
+mount /dev/mmcblk2p2 /mnt
 export QT_QPA_PLATFORM='eglfs'
 /usr/bin/mxapp2  -platform eglfs &
 
