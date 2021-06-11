113aab5a64f7531bdaae515c934656d991330d58
codebug8 <livelwh@outlook.com>
2021-06-11 03:37:22 +0000

    100ask stm32mp157 pro board add auto ota ,
    Signed-off-by: codebug8 <livelwh@outlook.com>

--- board/100ask/stm32mp157-100ask/overlay/systemD/etc/swupdate/postupdate.sh
+++ board/100ask/stm32mp157-100ask/overlay/systemD/etc/swupdate/postupdate.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
-
-LINE=$(cat /proc/cmdline )
-PART_STATUS=$(echo ${LINE:28:14} | awk '{print $1}')
-
-if [ $PART_STATUS = "/dev/mmcblk0p2" ]; then
-	NEXT_ROOTFS=/dev/mmcblk1p1
-else
-	NEXT_ROOTFS=/dev/mmcblk1p2
-fi
-
-# Add update marker
-mount ${NEXT_ROOTFS} /mnt
-touch /mnt/update-ok
-umount /mnt
-
+touch /update-ok
+sync
+wait
+echo -e "\033[32m System update ok please wait reboot system ! \033[0m"
 reboot
